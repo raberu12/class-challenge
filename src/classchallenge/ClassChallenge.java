cb98da713b0f41e5c14bc7d7ff290cd14b830040
raberu12 <114815628+raberu12@users.noreply.github.com>
2023-09-15 07:44:40 +0800

    added transfer

--- src/classchallenge/ClassChallenge.java
+++ src/classchallenge/ClassChallenge.java
@@ -10,28 +10,32 @@ package classchallenge;
 public class ClassChallenge {
 
     public static void main(String[] args) {
-        bank_account A = new bank_account(1234, 1000.0f, "Carlo", "carlo@yahoo.com", 911);
-        System.out.println(A);
+        bankAccount A = new bankAccount(1234, 1000.0f, "Carlo", "carlo@yahoo.com", 911);
+        bankAccount B = new bankAccount(1235, 5000.0f, "Jarlo", "Jarlo@yahoo.com", 912);
+        A.displayAccount();
         if (A.withdraw(700)) {
-            System.out.println("\nYour new balance is: $" + A.getAccount_balance());
+            System.out.println("\nYour new balance is: $" + A.getAccountBalance());
         } else {
             System.out.println("\nUnable to withdraw due to missing funds");
         }
         if (A.deposit(900)) {
-            System.out.println("\nYour new balance is: $" + A.getAccount_balance());
+            System.out.println("\nYour new balance is: $" + A.getAccountBalance());
         } else {
             System.out.println("\nUnable to deposit due to invalid funds");
         }
         if (A.withdraw(10000)) {
-            System.out.println("\nYour new balance is: $" + A.getAccount_balance());
+            System.out.println("\nYour new balance is: $" + A.getAccountBalance());
         } else {
             System.out.println("\nUnable to withdraw due to missing funds");
         }
         if (A.deposit(0)) {
-            System.out.println("\nYour new balance is: $" + A.getAccount_balance());
+            System.out.println("\nYour new balance is: $" + A.getAccountBalance());
         } else {
             System.out.println("\nUnable to deposit due to invalid funds");
         }
-
+        A.transfer(500, B);
+        A.displayAccount();
+        System.out.println("");
+        B.displayAccount();
     }
 }
